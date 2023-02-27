0125697e21e7ef923db21b87b3a91123b95da7c2
BTDMIS <122453016+BTDMIS@users.noreply.github.com>
2023-02-27 16:58:35 -0500

    4:58pm

---
+++ node_js_snippets.js
@@ -0,0 +1,25 @@
+    //console.log('app.use req.url: ' + req.url);
+    //console.log(req.body);
+
+    //res.sendFile(path.join(__dirname, 'public', 'index.html'))    // Note: /index.html gets returned AUTOMATICALLY. You don't have to add a res.sendFile()!
+
+
+    //if (req.url == "/start_menu_template"){
+    //    const str = CircularJSON.stringify(req);
+    //    const fs = require('fs');
+    //    fs.writeFileSync('./req.json', JSON.stringify(str, null, 2) , 'utf-8');
+    //}
+
+//const CircularJSON = require('circular-json')
+
+//app.post("/*",function(req,res,next){
+//    console.log("app.post - req.url " + req.url);
+//    console.log("app.post - req.body " + JSON.stringify(req.body));
+//    console.log("content-type: " + req.header('Content-Type'));
+//    res.send("response");
+//});
+
+    
+//app.get('/getme', (req, res) => {
+//    res.send('Hello');
+//});
